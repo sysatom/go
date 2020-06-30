89858a400bd52ba457ec5a8ff143aeded10bd886
sysatom <sysatom@gmail.com>
2020-06-30 12:13:43 +0800

    redis

--- redis/lock.go
+++ redis/lock.go
@@ -1,1 +1,23 @@
 package redis
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+func DistributedLock(ctx context.Context, client *redis.Client, key string, value interface{}, timeout time.Duration) (bool, error) {
+	return client.SetNX(ctx, key, value, timeout).Result()
+}
+
+func DistributedUnlock(ctx context.Context, client *redis.Client, key string) (int64, error) {
+	return client.Del(ctx, key).Result()
+}
+
+func NewRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
